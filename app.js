1dcd9bc5f372f5e5e55276a8ac35b839d3f55ed4
TalitaGabriela <talitagabriela1216@gmail.com>
2023-10-24 14:25:50 -0400

    Commit

--- app.js
+++ app.js
@@ -5,6 +5,19 @@ const port = 3000;
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
+const produtos = [
+  {id: 1, titulo: "", valor:"", descricao:"", imagem:""},
+  {id: 2, titulo: "", valor:"", descricao:"", imagem:""},
+  {id: 3, titulo: "", valor:"", descricao:"", imagem:""},
+  {id: 4, titulo: "", valor:"", descricao:"", imagem:""},
+  {id: 5, titulo: "", valor:"", descricao:"", imagem:""},
+  {id: 6, titulo: "", valor:"", descricao:"", imagem:""},
+  {id: 7, titulo: "", valor:"", descricao:"", imagem:""},
+  {id: 8, titulo: "", valor:"", descricao:"", imagem:""},
+  {id: 9, titulo: "", valor:"", descricao:"", imagem:""},
+  {id: 10, titulo: "", valor:"", descricao:"", imagem:""}
+]
+
 app.get('/', (req, res) => {
   res.render('index', { message: 'Olá, Mundo!' });
 });
